c613b5133ccadeadfe731d9c571a364fc82e3aeb
GaoXuSheng <moxiaojulove@foxmail.com>
2025-10-04 20:48:13 +0800

    support fancymenu background

--- common/src/main/java/top/yunmouren/craftbrowser/Craftbrowser.java
+++ common/src/main/java/top/yunmouren/craftbrowser/Craftbrowser.java
@@ -16,6 +16,7 @@ public final class Craftbrowser {
         if (Platform.getEnvironment() == Env.CLIENT) {
             Config.CLIENT.load();
             new BrowserProcess();
+
         }
     }
 }
--- forge/src/main/java/top/yunmouren/craftbrowser/fancymenu/BrowserMenuBackground.java
+++ forge/src/main/java/top/yunmouren/craftbrowser/fancymenu/BrowserMenuBackground.java
@@ -1,17 +1,19 @@
-package top.yunmouren.craftbrowser.client.browser;
+package top.yunmouren.craftbrowser.fancymenu;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
+import de.keksuccino.fancymenu.customization.background.MenuBackground;
+import de.keksuccino.fancymenu.customization.background.MenuBackgroundBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
+import top.yunmouren.craftbrowser.client.browser.BrowserManager;
+import top.yunmouren.craftbrowser.client.browser.BrowserRender;
 import top.yunmouren.craftbrowser.client.config.Config;
 
 import java.util.HashMap;
@@ -23,45 +25,45 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
-import static org.lwjgl.glfw.GLFW.GLFW_MOD_SHIFT;
-
-public abstract class AbstractWebScreen extends Screen {
+public class BrowserMenuBackground extends MenuBackground {
     private final Minecraft mc = Minecraft.getInstance();
     private int texWidth = mc.getWindow().getScreenWidth();
     private int texHeight = mc.getWindow().getScreenHeight();
-    public final BrowserManager browserManager = new BrowserManager();
-    private final Map<Integer, Long> heldKeys = new HashMap<>();
+    private final BrowserManager browserManager = new BrowserManager();
     private final BrowserRender browserRender = new BrowserRender();
+    public String url;
 
-    protected AbstractWebScreen(Component p_96550_) {
-        super(p_96550_);
+    public BrowserMenuBackground(MenuBackgroundBuilder<?> builder) {
+        super(builder);
         browserManager.resizeViewport(texWidth, texHeight);
+        browserManager.customizeLoadingScreenUrl();
     }
-
-    @Override
-    public void tick() {
-        super.tick();
-        long now = System.currentTimeMillis();
-        for (Map.Entry<Integer, Long> entry : heldKeys.entrySet()) {
-            int keyCode = entry.getKey();
-            long lastTime = entry.getValue();
-
-            if (now - lastTime >= Config.CLIENT.keyPressDelay.get()) {
-                browserManager.keyPress(keyCode, 0, false, true); // repeat
-                entry.setValue(now);
-            }
+    protected BrowserMenuBackground(MenuBackgroundBuilder<?> builder, @NotNull String url){
+        super(builder);
+        browserManager.resizeViewport(texWidth, texHeight);
+        loadUrl(url);
+    }
+    public void loadUrl(String url) {
+        this.url = url;
+        if (url.startsWith("[source:web]")) {
+            url = url.substring("[source:web]".length());
         }
-
+        browserManager.loadUrl(url);
     }
-
+    public void customizeLoadingScreenUrl(){
+        browserManager.customizeLoadingScreenUrl();
+    }
     @Override
-    public void render(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+    public boolean isFocusable() {
+        return false;
+    }
+    @Override
+    public void render(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partial) {
         var render = browserRender.render(texWidth, texHeight);
         if (render == 0) {
             return;
         }
-        RenderSystem.disableBlend();
+        RenderSystem.enableBlend();
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, render);
@@ -69,7 +71,7 @@ public abstract class AbstractWebScreen extends Screen {
         Tesselator tessellator = Tesselator.getInstance();
         BufferBuilder buffer = tessellator.getBuilder();
 
-        Matrix4f matrix = guiGraphics.pose().last().pose();
+        Matrix4f matrix = graphics.pose().last().pose();
         int guiWidth = mc.getWindow().getGuiScaledWidth();
         int guiHeight = mc.getWindow().getGuiScaledHeight();
         buffer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
@@ -78,19 +80,12 @@ public abstract class AbstractWebScreen extends Screen {
         buffer.vertex(matrix, guiWidth, 0, 0).uv(1, 0).endVertex();
         buffer.vertex(matrix, 0, 0, 0).uv(0, 0).endVertex();
         tessellator.end();
-
-        RenderSystem.enableDepthTest();
-        super.render(guiGraphics, mouseX, mouseY, partialTicks);
     }
-
-
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private ScheduledFuture<?> pendingResizeTask = null;
 
     @Override
-    public void resize(Minecraft minecraft, int width, int height) {
-        super.resize(minecraft, width, height);
-
+    public void onAfterResizeScreen() {
         if (pendingResizeTask != null && !pendingResizeTask.isDone()) {
             pendingResizeTask.cancel(false);
         }
@@ -102,13 +97,6 @@ public abstract class AbstractWebScreen extends Screen {
         }, RESIZE_DELAY_MS, TimeUnit.MILLISECONDS);
     }
 
-    /**
-     * 将 Minecraft GUI 坐标转换为窗口像素坐标
-     *
-     * @param guiX GUI 坐标 X
-     * @param guiY GUI 坐标 Y
-     * @return 长度为 2 的数组 [pixelX, pixelY]
-     */
     public static int[] guiToPixel(double guiX, double guiY) {
         Minecraft mc = Minecraft.getInstance();
         int windowWidth = mc.getWindow().getScreenWidth();   // 实际像素宽
@@ -121,22 +109,20 @@ public abstract class AbstractWebScreen extends Screen {
 
         return new int[]{pixelX, pixelY};
     }
-
+    private final Set<Integer> heldMouseButtons = new HashSet<>();
     @Override
     public void mouseMoved(double mouseX, double mouseY) {
         int[] pos = guiToPixel(mouseX, mouseY);
         boolean dragging = !heldMouseButtons.isEmpty();
         browserManager.mouseMove(pos[0], pos[1], dragging);
+        super.mouseMoved(mouseX, mouseY);
     }
-
-    private final Set<Integer> heldMouseButtons = new HashSet<>();
-
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
         int[] pos = guiToPixel(mouseX, mouseY);
         browserManager.mousePress(pos[0], pos[1], button);
         heldMouseButtons.add(button); // 记录按下
-        return true;
+        return super.mouseClicked(mouseX, mouseY, button);
     }
 
     @Override
@@ -144,45 +130,30 @@ public abstract class AbstractWebScreen extends Screen {
         int[] pos = guiToPixel(mouseX, mouseY);
         browserManager.mouseRelease(pos[0], pos[1], button);
         heldMouseButtons.remove(button); // 移除按下记录
-        return true;
+        return super.mouseReleased(mouseX, mouseY, button);
     }
-
     @Override
     public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
         int[] pos = guiToPixel(mouseX, mouseY);
         browserManager.mouseWheel(pos[0], pos[1], (int) (-delta * Config.CLIENT.scrollWheelPixels.get()));
-        return true;
+        return super.mouseScrolled(mouseX, mouseY, delta);
     }
-
+    private final Map<Integer, Long> heldKeys = new HashMap<>();
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (keyCode == GLFW_KEY_ESCAPE && (modifiers & GLFW_MOD_SHIFT) != 0) {
-            this.onClose();
-            return true;
-        }
-
         browserManager.keyPress(keyCode, modifiers, false, false);
         heldKeys.put(keyCode, System.currentTimeMillis());
-
-        return true;
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
     @Override
     public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
-        if (keyCode == GLFW_KEY_ESCAPE && (modifiers & GLFW_MOD_SHIFT) != 0) {
-            this.onClose();
-            return true;
-        }
-
         browserManager.keyPress(keyCode, modifiers, true, false);
         heldKeys.remove(keyCode);
-        return true;
+        return super.keyReleased(keyCode, scanCode, modifiers);
     }
-
     @Override
-    public void onClose() {
-        browserManager.customizeLoadingScreenUrl();
-        heldKeys.clear();
-        Minecraft.getInstance().setScreen(null);
+    public NarrationPriority narrationPriority() {
+        return NarrationPriority.NONE;
     }
 }
