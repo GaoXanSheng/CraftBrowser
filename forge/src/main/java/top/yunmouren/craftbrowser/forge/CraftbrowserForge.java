c9318ce86ae37e1bae597d1c810a1c7357b5e724
GaoXuSheng <moxiaojulove@foxmail.com>
2025-10-10 23:35:56 +0800

    feat: Add FancyMenu integration and code cleanup

--- forge/src/main/java/top/yunmouren/craftbrowser/forge/CraftbrowserForge.java
+++ forge/src/main/java/top/yunmouren/craftbrowser/forge/CraftbrowserForge.java
@@ -1,14 +1,10 @@
 package top.yunmouren.craftbrowser.forge;
 
-import de.keksuccino.fancymenu.customization.background.MenuBackgroundRegistry;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import top.yunmouren.craftbrowser.Craftbrowser;
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import top.yunmouren.craftbrowser.fancymenu.BrowserMenuBackgroundBuilder;
 import top.yunmouren.craftbrowser.proxy.ClientProxy;
 import top.yunmouren.craftbrowser.proxy.IProxy;
 import top.yunmouren.craftbrowser.proxy.ServerProxy;
@@ -24,13 +20,6 @@ public final class CraftbrowserForge {
         EventBuses.registerModEventBus(Craftbrowser.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         Craftbrowser.init();
         proxy.init();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
     }
-    private void onClientSetup(FMLClientSetupEvent event) {
-        if (ModList.get().isLoaded("fancymenu")) {
-            MenuBackgroundRegistry.register(new BrowserMenuBackgroundBuilder("NCEF"));
-        } else {
-            Craftbrowser.LOGGER.info("FancyMenu not loaded, skipping BrowserMenuBackground registration.");
-        }
-    }
+
 }
--- forge/src/main/java/top/yunmouren/craftbrowser/proxy/ClientProxy.java
+++ forge/src/main/java/top/yunmouren/craftbrowser/proxy/ClientProxy.java
@@ -2,6 +2,9 @@ package top.yunmouren.craftbrowser.proxy;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import top.yunmouren.craftbrowser.server.Server;
 
 @OnlyIn(Dist.CLIENT)
@@ -9,5 +12,13 @@ public class ClientProxy implements IProxy {
     @Override
     public void init() {
         new Server();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event) {
+        if (ModList.get().isLoaded("fancymenu")) {
+            de.keksuccino.fancymenu.customization.background.MenuBackgroundRegistry
+                    .register(new top.yunmouren.craftbrowser.fancymenu.BrowserMenuBackgroundBuilder("NCEF"));
+        }
     }
 }
