223ee64a099c8bf0f7d32db5d0c2b1565a26d0a2
GaoXuSheng <moxiaojulove@foxmail.com>
2025-11-27 23:57:00 +0800

    refactor: remove unused client setup logic and fancymenu compatible

--- forge/src/main/java/top/yunmouren/craftbrowser/proxy/ClientProxy.java
+++ forge/src/main/java/top/yunmouren/craftbrowser/proxy/ClientProxy.java
@@ -2,30 +2,13 @@ package top.yunmouren.craftbrowser.proxy;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import top.yunmouren.craftbrowser.Craftbrowser;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientProxy extends CommonProxy {
     @Override
     public void init() {
         super.init();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);
-    }
-
-    private void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            if (ModList.get().isLoaded("fancymenu")) {
-                try {
-                    de.keksuccino.fancymenu.customization.background.MenuBackgroundRegistry.register(new top.yunmouren.craftbrowser.fancymenu.BrowserMenuBackgroundBuilder("NCEF"));
-                    Craftbrowser.LOGGER.info("Registered FancyMenu background integration");
-                } catch (Exception e) {
-                    Craftbrowser.LOGGER.error("Failed to register FancyMenu integration", e);
-                }
-            }
-        });
     }
 }
